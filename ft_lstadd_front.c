6a79cedb8da5866af92e1f7e298811bd6b64059e
lipemecprec <lipemecprec@yahoo.com.br>
2021-08-29 12:31:15 -0300

    lstadd_front

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,13 +6,13 @@
 /*   By: faguilar <faguilar@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/28 12:14:02 by faguilar          #+#    #+#             */
-/*   Updated: 2021/08/28 12:40:05 by faguilar         ###   ########.fr       */
+/*   Updated: 2021/08/28 23:43:54 by faguilar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_lstadd_front(t_list **lst, t_list *new)
+void	ft_lstadd_front(t_list **lst, t_list *new)
 {
 	if (!*lst)
 	{
