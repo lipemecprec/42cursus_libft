248cd9d9449a06bb3b82acf3f3683604f1687e20
lipemecprec <lipemecprec@yahoo.com.br>
2021-08-29 12:30:38 -0300

    lstadd_back

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: faguilar <faguilar@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/28 13:22:17 by faguilar          #+#    #+#             */
-/*   Updated: 2021/08/28 16:40:22 by faguilar         ###   ########.fr       */
+/*   Updated: 2021/08/29 00:14:56 by faguilar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,7 @@
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	if (!*lst)
-	{
 		*lst = new;
-	}
 	else
-	{
 		ft_lstlast(*lst)->next = new;
-	}
 }
