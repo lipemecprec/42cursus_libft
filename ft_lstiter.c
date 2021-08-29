950bdbfb8d4345fd261a57780286a9fac1205045
lipemecprec <lipemecprec@yahoo.com.br>
2021-08-29 12:33:45 -0300

    lstinter

---
+++ ft_lstiter.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: faguilar <faguilar@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/29 11:21:22 by faguilar          #+#    #+#             */
+/*   Updated: 2021/08/29 11:50:24 by faguilar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	t_list	*curr;
+
+	if (!lst)
+		return ;
+	curr = lst;
+	while (curr != NULL)
+	{
+		f(curr->content);
+		curr = curr->next;
+	}
+}
