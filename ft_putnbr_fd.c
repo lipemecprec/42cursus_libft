4088d8ef2684d13d7fe2fa671bae2bb514c9d1a7
lipemecprec <lipemecprec@yahoo.com.br>
2021-09-01 00:13:52 -0300

    putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,16 +6,31 @@
 /*   By: faguilar <faguilar@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/25 19:43:43 by faguilar          #+#    #+#             */
-/*   Updated: 2021/08/27 23:40:53 by faguilar         ###   ########.fr       */
+/*   Updated: 2021/08/31 23:13:57 by faguilar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static void	ft_putdigit_fd(int n, int fd)
+{
+	char	c;
+
+	c = n + '0';
+	ft_putchar_fd(c, fd);
+}
+
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*nbr;
+	long int	nbr;
 
-	nbr = ft_itoa(n);
-	ft_putstr_fd(nbr, fd);
+	nbr = (long int)n;
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		nbr = -nbr;
+	}
+	if (nbr >= 10)
+		ft_putnbr_fd(nbr / 10, fd);
+	ft_putdigit_fd(nbr % 10, fd);
 }
