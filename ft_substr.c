5ae40ed2fff6356d7de36e5641415ba52838958b
lipemecprec <lipemecprec@yahoo.com.br>
2021-09-01 20:38:40 -0300

    substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: faguilar <faguilar@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/22 13:16:22 by faguilar          #+#    #+#             */
-/*   Updated: 2021/08/31 00:12:08 by faguilar         ###   ########.fr       */
+/*   Updated: 2021/09/01 20:07:44 by faguilar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,22 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub;
-	size_t	remmlen;
+	size_t	remmsub;
 
 	if (!s)
 		return (NULL);
-	remmlen = ft_strlen(s) - start;
-	if (remmlen >= len)
-		sub = (char *)ft_calloc(sizeof(char), len + 1);
-	else
-		sub = (char *)ft_calloc(sizeof(char), remmlen + 1);
-	if (!sub)
-		return (NULL);
-	if (ft_strlen(s) > start)
+	remmsub = ft_strlen(s) - start;
+	if (start < ft_strlen(s))
+	{
+		if (remmsub > len)
+			sub = (char *)ft_calloc(sizeof(char), len + 1);
+		else
+			sub = (char *)ft_calloc(sizeof(char), remmsub + 1);
+		if (!sub)
+			return (NULL);
 		ft_strlcpy(sub, &s[start], len + 1);
+	}
+	else
+		sub = (char *)ft_calloc(1, 1);
 	return (sub);
 }
