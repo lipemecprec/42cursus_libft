56997f9883f64500a5165afd0fea0bb900912a84
lipemecprec <lipemecprec@yahoo.com.br>
2021-09-01 20:49:26 -0300

    strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: faguilar <faguilar@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/18 23:19:17 by faguilar          #+#    #+#             */
-/*   Updated: 2021/09/01 00:40:30 by faguilar         ###   ########.fr       */
+/*   Updated: 2021/09/01 20:49:01 by faguilar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,9 @@ char	*ft_strchr(const char *s, int c)
 	int	i;
 
 	i = 0;
-	if (!s)
-		return (NULL);
 	while (s[i] != '\0')
 	{
-		if (s[i] == c)
+		if ((unsigned char)s[i] == (unsigned char)c)
 			return ((char *)&s[i]);
 		i++;
 	}
