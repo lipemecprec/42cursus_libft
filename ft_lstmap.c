9399ddcab3dd3d4ffb0b6bc9fb3e351d92b2c01d
lipemecprec <lipemecprec@yahoo.com.br>
2021-09-02 21:48:27 -0300

    lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: faguilar <faguilar@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/29 12:21:41 by faguilar          #+#    #+#             */
-/*   Updated: 2021/08/29 13:40:55 by faguilar         ###   ########.fr       */
+/*   Updated: 2021/09/02 21:47:52 by faguilar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,10 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	**mapped;
 	t_list	*item;
 
-	mapped = malloc(sizeof(t_list *));
 	if (!lst)
+		return (NULL);
+	mapped = ft_calloc(sizeof(t_list *), 1);
+	if (!mapped)
 		return (NULL);
 	while (lst != NULL)
 	{
