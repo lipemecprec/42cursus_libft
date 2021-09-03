54cb750c9618ead62f1ed208641a6d3b0397c8aa
lipemecprec <lipemecprec@yahoo.com.br>
2021-09-02 21:31:06 -0300

    striteri

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: faguilar <faguilar@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/25 18:38:05 by faguilar          #+#    #+#             */
-/*   Updated: 2021/08/25 19:23:26 by faguilar         ###   ########.fr       */
+/*   Updated: 2021/09/01 00:08:45 by faguilar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 	unsigned int	i;
 
 	i = 0;
+	if (!s)
+		return ;
 	while (s[i] != '\0')
 	{
 		f(i, &s[i]);
